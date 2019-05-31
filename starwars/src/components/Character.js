06acfe1492940e8d29ee95442c80b428d7e7e717
Jose Montero Jr <josemariodev@gmail.com>
2019-05-31 10:57:56 -0500

    character.js displaying characters

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import './StarWars.scss';
 
-const Character = () => {
+const Character = props => {
   return (
-    <div>
-      
+    <div className='character-container'>
+      <h1>Name:</h1>
+      <p> {props.Character.name}</p>
+      <h1>Gender:</h1>
+      <p> {props.Character.gender}</p>
+      <h1>Hair Color:</h1>
+      <p> {props.Character.hair_color}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Character
+export default Character;
