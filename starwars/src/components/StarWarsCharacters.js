297ed56a9cfca7b4dd8f8c29a3219aabebd47cfb
Jose Montero Jr <josemariodev@gmail.com>
2019-05-31 10:37:49 -0500

    mapping over starwarschars

--- starwars/src/components/StarWarsCharacters.js
+++ starwars/src/components/StarWarsCharacters.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Character from './Character';
 
+//mapping over data, sending it to Character.js via props
+
 const StarWarsCharacters = props => {
   return props.StarWarsCharacters.map(character => (
     <Character Character={character} />
